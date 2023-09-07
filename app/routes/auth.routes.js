71d3361dba41790fcd7656de3e63bd159f0c2775
vdat362002 <vandatdinh2@gmail.com>
2023-09-07 18:28:47 +0700

    up

--- app/routes/auth.routes.js
+++ app/routes/auth.routes.js
@@ -4,13 +4,13 @@ const controller = require("../controllers/auth.controller");
 const app = require("express").Router();
 
 module.exports = function() {
-  // app.use(function(req, res, next) {
-  //   res.header(
-  //     "Access-Control-Allow-Headers",
-  //     "x-access-token, Origin, Content-Type, Accept"
-  //   );
-  //   next();
-  // });
+  app.use(function(req, res, next) {
+    res.header(
+      "Access-Control-Allow-Headers",
+      "x-access-token, Origin, Content-Type, Accept"
+    );
+    next();
+  });
 
   app.post(
     "/auth/signup",
