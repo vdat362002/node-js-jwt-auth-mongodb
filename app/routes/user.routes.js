958a536379ac98822c023c802468dcb405916b74
vdat362002 <vandatdinh2@gmail.com>
2023-09-07 23:15:13 +0700

    up

--- app/routes/user.routes.js
+++ app/routes/user.routes.js
@@ -3,14 +3,14 @@ const controller = require("../controllers/user.controller");
 
 const app = require("express").Router();
 
-module.exports = function() {
-  app.use(function(req, res, next) {
-    res.header(
-      "Access-Control-Allow-Headers",
-      "x-access-token, Origin, Content-Type, Accept"
-    );
-    next();
-  });
+// module.exports = function() {
+//   app.use(function(req, res, next) {
+//     res.header(
+//       "Access-Control-Allow-Headers",
+//       "x-access-token, Origin, Content-Type, Accept"
+//     );
+//     next();
+//   });
 
   app.get("/test/all", controller.allAccess);
 
