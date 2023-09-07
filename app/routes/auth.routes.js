801b39fbca5620690d22aae100940779ad5033e1
vdat362002 <vandatdinh2@gmail.com>
2023-09-07 17:56:45 +0700

    ew

--- app/routes/auth.routes.js
+++ app/routes/auth.routes.js
@@ -11,7 +11,7 @@ module.exports = function(app) {
   // });
 
   app.post(
-    "/api/auth/signup",
+    "/auth/signup",
     [
       verifySignUp.checkDuplicateUsernameOrEmail,
       verifySignUp.checkRolesExisted
