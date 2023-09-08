27e69ed1e179d3f806b1f021742e1c16c6495432
vdat362002 <vandatdinh2@gmail.com>
2023-09-08 22:03:55 +0700

    up

--- server.js
+++ server.js
@@ -2,15 +2,6 @@ const app = require("./app/app");
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
-const server = app.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
-});
-
-
-process.on("SIGINT", () => {
-  console.info("SIGINT signal received.");
-  console.log("Closing http server.");
-  server.close(() => {
-    console.log("Http server closed.");
-  });
 });
