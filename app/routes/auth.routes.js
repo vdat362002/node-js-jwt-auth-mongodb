13df8aea8c654342eccb7e4c39d98417be4eda96
vdat362002 <vandatdinh2@gmail.com>
2023-09-08 23:39:51 +0700

    test

--- app/routes/auth.routes.js
+++ app/routes/auth.routes.js
@@ -4,10 +4,6 @@ const controller = require("../controllers/auth.controller");
 const app = require("express").Router();
 
   app.use(function(req, res, next) {
-    res.header(
-      "Access-Control-Allow-Headers",
-      "x-access-token, Origin, Content-Type, Accept"
-    );
     next();
   });
 
--- app/routes/user.routes.js
+++ app/routes/user.routes.js
@@ -4,10 +4,7 @@ const controller = require("../controllers/user.controller");
 const app = require("express").Router();
 
   app.use(function(req, res, next) {
-    res.header(
-      "Access-Control-Allow-Headers",
-      "x-access-token, Origin, Content-Type, Accept"
-    );
+    res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
     next();
   });
 
